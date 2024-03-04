4037bd1944550a3fda378218ee52409107667dd0
Tyler Ruff <truff@blazed.work>
2024-03-04 06:15:31 -0500

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), partytown(), icon()],
 });
